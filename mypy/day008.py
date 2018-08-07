880791f04c90a0a7b35848786f193b26447d334b
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-08-07 21:00:23 +0530

    controlstatement

---
+++ mypy/day008.py
@@ -0,0 +1,38 @@
+#1
+#boolean
+
+my_boolean = True
+print(my_boolean)
+
+print(2 == 3)
+
+print("hello" == "hello")
+ 
+ # Output
+#True
+#False
+#True
+
+
+#2
+#string concatenation
+print("First string" + ", " + "second string")
+
+#output
+#Fisrt string, second string
+
+
+#3
+#type conversion
+print("2" + "3")
+
+print(int("2") + int("3"))
+
+#output
+#23 simply concatenate due to string type
+#5  add due to type conversion from str to int
+
+
+
+
+
