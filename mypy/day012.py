56a9ec790594b9c01df78ccb4030d399557e415d
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-08-11 23:26:49 +0530

    elif statement

---
+++ mypy/day012.py
@@ -0,0 +1,17 @@
+#The "elif" (short for else if) statement is a shortcut to use in place of if and else statements.
+
+#A series of if elif statements can have a "else" block at the end, which is use when all of the if or elif expressions is "false".
+
+num = 7
+if num == 5:
+    print("Number is five")
+elif num == 11:
+    print("Number is eleven")
+elif num == 7:
+    print("Number is seven")
+else:
+    print("Number isn't five, eleven or seven")
+
+
+#output 
+#Number is seven 
