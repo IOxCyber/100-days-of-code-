3122ad7d76a5df417dfa0e6fc6e019731d028462
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-08-15 19:18:14 -0700

    arrayProperties

---
+++ Mycodehere/day015.js
@@ -0,0 +1,26 @@
+// Array properties:
+//length:
+
+var courses = ["HTML", "CSS", "JS"];
+document.write(courses.length+",<br>");
+
+/*
+The length property is always one more than the highest array index.
+If the array is empty, the length property returns 0.
+*/
+
+//concate:
+
+var c1 = ["HTML", "CSS"];
+var c2 = ["JS", "C++"];
+var courses = 
+c1.concat(c2);
+
+/* The concat operation does not affect the c1 and c2 arrays - it returns the resulting concatenation as a new array.
+ output */
+ 3
+ JS
+
+
+
+
