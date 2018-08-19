1aa0d21fc5c2ef90f530d54eb7af161d6c4f3a5f
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-08-19 10:32:31 -0700

    ex. of logical operator

---
+++ mypy/day018.py
@@ -0,0 +1,30 @@
+#Example of AND, OR, NOT
+
+print(1 == 1 and 2 == 2)
+
+print(1 != 1 or 2 == 3)
+
+print(not 1 > 7)
+
+print(5 == 8 or 9 != 9)
+
+print(not 2 == 3)
+
+print(1 != 1 or 2 == 2)
+
+print(2 > 1 and 3 <  6)
+
+# Boolean logic is used to make more complicated conditions for if statements that rely on more than one condition.
+#  not only takes one argument, and inverts it. 
+# The result of not True is False, and not False goes to True.
+
+
+#OUTPUT
+
+# True
+# False
+# True
+# False
+# True
+# True
+# True
