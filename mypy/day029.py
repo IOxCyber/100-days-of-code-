908c8a5250aeb44e5ada09546d7c46843e610f2a
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-03 10:25:21 -0700

    use of range method

---
+++ mypy/day029.py
@@ -0,0 +1,18 @@
+#RANGE
+
+#1
+numbers = list(range(10)) #print digits from 0 to 9
+print(numbers)
+
+#2
+numbers = list(range(3, 8))  #print from 2 to 7
+print(numbers)
+
+#3
+numbers = list(range(5, 20, 3))  #here the last argument used for ITERVAL in the numbers.
+print(numbers)
+
+
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [3, 4, 5, 6, 7]
+# [5, 8, 11, 14, 17]
