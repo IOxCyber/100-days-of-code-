961ef636b99b5daf41fcbb2db770e990500bb280
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-05 11:28:55 -0700

    user define function

---
+++ mypy/day031.py
@@ -0,0 +1,7 @@
+def hello():  # def is used to define a user define function
+    print("Hello world!") #write in the same way like just below the hello name
+    
+    hello()
+#output
+#Hello world!
+
