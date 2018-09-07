e7382a9dced8da2d3c47da1fa993d1cceb9ec901
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-06 18:04:47 -0700

    Update day031.py

--- mypy/day031.py
+++ mypy/day031.py
@@ -1,7 +1,30 @@
+#1
 def hello():  # def is used to define a user define function
     print("Hello world!") #write in the same way like just below the hello name
     
     hello()
 #output
 #Hello world!
+#2
+def print_this(word):
+  print("This is " + word+".")
+print_this("spam")
+print_this("eggs")
+print_this("python")
 
+#3 
+def print_calc(x, y):    
+  print(x - y)
+  print(x * y)   
+  print(x + y)
+  
+print_calc(8, 3)  
+
+#here we have taken two arg with that we're calculating sum,sub,multiply operation.  
+#output
+#This is spam.
+#This is eggs.
+#This is python.
+#5
+#24
+#11
