1934e7d3fc030092d025d7d6a094c8fbfdf03239
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-16 11:00:35 -0700

    Returning from Function

--- mypy/day032.py
+++ mypy/day032.py
@@ -1,1 +1,16 @@
 # Returning from Function
+#Here we have defined the function called as "max"
+#and Called the function 2 times i.e in print() and to the Variable Z too. 
+
+def max(x, y):
+    if x >=y:
+        return x
+    else:
+        return y
+        
+print(max(4, 7))
+z = max(8, 5)
+print(z)
+#output
+#7
+#8
