594242c42c063d653755e99f2d9c58fcbbabe606
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-17 11:59:53 -0700

    function reffer to a VAR

--- mypy/day034.py
+++ mypy/day034.py
@@ -1,1 +1,12 @@
 # Functions as Objects
+def multiply(x, y):
+    return x * y
+
+a = 7
+b = 4
+operation = multiply
+print(operation(a, b))
+
+#functions are just like any other kind of value. 
+#They can be assigned and reassigned to variables, and later referenced by those names.
+
