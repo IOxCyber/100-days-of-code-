c19df0f488be1b63ce254bf98ce09430cb31f0f7
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-17 11:05:02 -0700

    Update day033.py

--- mypy/day033.py
+++ mypy/day033.py
@@ -1,1 +1,12 @@
 #Comments & Docstrings
+def show(word):
+    """
+    Print a word with an
+    exclamation mark following it.
+    """
+    print("This is "+word+" !")
+
+show("spam")
+"""
+we have called function as show() with string arg "spam" & this args replace the VAR word with this arg "spam" 
+"""
