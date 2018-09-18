a670be83cd2a8a52ad40c2c5ee72ed53ad9cdfeb
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-18 06:35:05 -0700

    random module

--- mypy/day036.py
+++ mypy/day036.py
@@ -1,1 +1,20 @@
-#new file
+import random #here we have import a method i.e ramdom to print random numbers.
+
+for i in range(5):
+    
+    print(random.randint(1, 9))
+    
+    """
+    Modules are pieces of code.
+    
+    People have written to fulfill common tasks &
+    such as generating random numbers ,
+    performing mathematical operations.
+    
+    The code uses the randint function defined in the random module to print
+    random numbers.
+    
+    The basic way to use a module is to add import module_name at the top of your code,
+    and then using module_name.var to access functions. 
+    
+    """
