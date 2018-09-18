ff2b7d3048cb0621ece6873a3a89d87b2a41d420
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-18 06:09:36 -0700

    function as a args

--- mypy/day035.py
+++ mypy/day035.py
@@ -1,1 +1,12 @@
-# The Standard Library & pip
+def add(x, y):
+    return x + y
+
+def do_twice(arg, x, y): #we have added the func here as args
+    return arg(arg(x, y), arg(x, y))
+
+a = 5
+b = 10
+
+print(add(a, b))
+print(do_twice(add, a, b))
+# here in the print fun the actual args replace the formal args. 
