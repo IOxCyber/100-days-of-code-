bb22a8550dd3d35e757250e9a63270bd7fdb15e2
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-18 10:30:03 -0700

    use of (from,as) when import a module

--- mypy/day37.py
+++ mypy/day37.py
@@ -1,1 +1,17 @@
-after ct1
+
+from math import pi
+from math import sqrt as square_root
+
+print(pi)
+print(square_root(100))
+"""
+Trying to import a module that isn't available causes an ImportError.
+You can import a module or object under a different name using the as keyword. 
+
+
+output
+
+3.141592653589793
+10.0
+
+"""
