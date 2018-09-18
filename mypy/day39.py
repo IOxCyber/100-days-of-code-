32a4e714a7d2e1c10eb622b7bc918da09a85a722
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-18 13:08:15 -0700

    Error Handling

--- mypy/day39.py
+++ mypy/day39.py
@@ -1,1 +1,34 @@
+#Error handling i.e Try and Except
+try:
+    num1 = 8
+    num2 =6 
+    print (num1 / num2)
+    print(num1//num2)
+    print(num1 % num2 )
+    print("Done calculation\n\n")
+except ZeroDivisionError:
+    print("An error occurred")
+    print("due to zero division")
+    
+    
+    
+try:
+   variable = 10
+   print (10 / 0) 
+except ZeroDivisionError:
+    print("Error")
+    print("Finished")
+    
+    """
+    Output 
+    
+1.3333333333333333
+1
+2
+Done calculation
 
+
+Error
+Finished
+    
+    """
