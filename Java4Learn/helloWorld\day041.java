fd0626bac48048452d9ee46b22e7e10f117b5b43
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-21 08:30:02 -0700

    Update helloWorld\day041.java

--- Java4Learn/helloWorld\day041.java
+++ Java4Learn/helloWorld\day041.java
@@ -12,4 +12,17 @@ public class First
  static: method can be run without creating an instance of the class containing the main method
  void: method doesn't return any value
  main: the name of the method
- */
+ 
+
+
+In Java, Every line of code that can actually run needs to be inside a class. we named the class "First".
+Every program in Java must have a class.
+Every Java program starts from the main method.
+The println method prints a line of text to the screen. 
+The System class and its out stream are used to access the println method.
+Each code statement mudt end with the semicolan.
+*/
+// Comments in Java
+ // This is single line comment.
+/* This is MultiLine Comment*/
+/** This is a documentation comment */
