ed13242035b1c408f61368186d370cc1a1884efc
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-21 08:28:08 -0700

    Update helloWorld\day041.java

--- Java4Learn/helloWorld\day041.java
+++ Java4Learn/helloWorld\day041.java
@@ -2,10 +2,14 @@ public class First
 {
 	public static void main(String[] args)
 {
-		System.out.println("Hello World");	
+		System.out.println("Hello World! I am Learning JAVA.");	
 }
 }
 
-//Remember that the name of the class and file name should be same eg. here it's First so file name must be first.java. 
-
-
+/* 
+ Remember that the name of the class and file name should be same eg. here it's First so file name must be first.java. 
+ public: anyone can access it
+ static: method can be run without creating an instance of the class containing the main method
+ void: method doesn't return any value
+ main: the name of the method
+ */
