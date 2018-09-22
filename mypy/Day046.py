301119d2fbbf043805ad804a66c42fdccd561498
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-22 11:04:21 -0700

    open a file

---
+++ mypy/Day046.py
@@ -0,0 +1,18 @@
+open("filename.txt") #Open file.
+
+open("filename.txt", "w") #Open file in write mode. 
+
+open("filename.txt", "r") #Open file in Read mode.
+
+
+open("filename.txt", "wb") #Open file in binary write mode
+
+file = open("filename.txt", "w") #Open fie in write mode & store it in file VAR. 
+
+file.close() #Close the file.
+
+
+"""
+You can use the + sign with each of the modes above to give them extra access to files.
+For example, r+ opens the file for both reading and writing.
+"""
