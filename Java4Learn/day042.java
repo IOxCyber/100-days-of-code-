27f8c35d2d576026a3d45ed012707dd7220b05de
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-22 13:02:12 -0700

    Update day042.java

--- Java4Learn/day042.java
+++ Java4Learn/day042.java
@@ -1,1 +1,14 @@
-#missed today
+#In.Dec.
+public class Program {
+    public static void main(String[] args) {
+        int a = 33;
+        int b = --a; //32
+        int c = a--; //32
+        int d = ++a; //32
+        int e = a++; //32
+        int f = a;   //33
+
+        System.out.println(b+"\n"+c+"\n"+d+"\n"+e+"\n"+f);
+    }
+}
+
