ff676fc87994d93011c649ddb4296b70111290ad
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-22 13:12:13 -0700

    string DT in java

--- Java4Learn/day043.java
+++ Java4Learn/day043.java
@@ -1,1 +1,15 @@
+/* A String is an object that represents a sequence of characters.
+You are allowed to define an empty string. For example, String str = "";
+The + (plus) operator between strings adds them together to make a new string n called concatenation.
+*/
 
+public class Program {
+    public static void main(String[] args) {
+        String firstName, lastName;
+        firstName = "KHAN";
+        lastName = "DANI";
+        
+        System.out.println("My name is " + firstName +" "+lastName);
+        
+    }
+}
