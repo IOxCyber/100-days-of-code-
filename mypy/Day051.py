a0b65446d3988fc1c0f3188a2070f09ad60b7102
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-09-27 10:27:47 -0700

    Tuples

---
+++ mypy/Day051.py
@@ -0,0 +1,24 @@
+#Tuples are very similar to lists, except that they are immutable (they cannot be changed). 
+#You can access the values in the tuple with their index, just as you did with lists:
+
+words = ("Give", "Me", "More!",)
+print(words) #print entire value
+#by index
+print(words[1])
+
+
+
+
+
+
+
+
+
+"""
+# list
+list =["one", "two"]
+# dictionary 
+dict ={1:"one", 2:"two"}
+ # tuple 
+tp =("one", "two")
+""" 
