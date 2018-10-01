7a690c0e0ae708a76c9c4dff960a8d7eeb25c462
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-10-01 10:51:14 -0700

    calc.

---
+++ PrePHP/Day054.php
@@ -0,0 +1,13 @@
+<?php
+    function fun($num1, $num2) // arguments
+    {
+        echo $num1 * $num2."<br>";
+        echo $num1 + $num2."<br>" ;
+        echo $num1 - $num2."<br>";
+        echo $num1 / $num2."<br>" ;
+        echo $num1 % $num2 ;
+    }
+    
+    fun(15,3); //parameters
+    
+?>
