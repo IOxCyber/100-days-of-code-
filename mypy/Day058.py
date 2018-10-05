465199f824662c1e203588fabe5ce56257dfaf9a
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-10-05 21:09:05 +0530

    predefine function in python

---
+++ mypy/Day058.py
@@ -0,0 +1,35 @@
+#string predefine functions
+     print("@".join(["spam", "eggs", "ham"]))
+     #prints spam@eggs@ham
+
+     print("Hello ME".replace("ME", "world"))
+     #prints Hello world
+
+     print("That is a sentence.".startswith("This"))
+     # prints False
+
+     print("This is a sentence.".endswith("sentence."))
+     # prints True
+
+     print("This is a sentence.".upper())
+     #prints THIS IS A SENTENCE.
+
+     print("AN ALL CAPS SENTENCE".lower())
+     #prints an all caps sentence
+
+     print("spam, eggs, ham".split(", "))
+     #prints ['spam', 'eggs', 'ham']
+
+#Numbers predefine functions
+    print(min(1, 2, 3, 4, 0, 2, 1))
+    print(max([1, 4, 9, 2, 5, 6, 8]))
+    print(abs(-99))
+    print(abs(42))
+    print(sum([1, 2, 3, 4, 5]))
+
+
+"""
+join() take list as arg,return the value by given symbol.
+"""
+
+
