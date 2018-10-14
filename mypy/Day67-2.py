f9b9e4edb1561dbaf7817bd975c92a68b3748a96
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-10-14 07:45:00 -0700

    Create Day67-2.py

---
+++ mypy/Day67-2.py
@@ -0,0 +1,13 @@
+mygenerator = (x**2 for x in range(3))
+for i in mygenerator:
+   print(i)
+   
+for i in mygenerator:
+   print(i)
+   
+"""
+ you can not perform for i in mygenerator a second time since 
+ generators can only be used once: 
+"""   
+   
+   
