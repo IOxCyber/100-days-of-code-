54ead27bc6b8625a3619d21c8ad18c2e6abf55d1
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-10-19 23:35:03 +0530

    Update Day069.py

--- mypy/Day069.py
+++ mypy/Day069.py
@@ -1,1 +1,14 @@
-dfg
+first = {1, 2, 3, 4, 5, 6}
+second = {4, 5, 6, 7, 8, 9}
+
+print(first | second)
+print(first & second)
+print(first - second)
+print(second - first)
+print(first ^ second)
+
+num_set  = {1, 2, 3, 4, 5}
+word_set = set(["spam", "eggs", "sausage"])
+
+print(3 in num_set)
+print("spam" not in word_set)d 
