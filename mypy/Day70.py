b8509fc3b11b23a2222a37b42901e44e9a8d7a2b
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-10-19 23:37:44 +0530

    recursion

--- mypy/Day70.py
+++ mypy/Day70.py
@@ -1,1 +1,18 @@
-fghfhg
+
+#Example of Recursion
+
+
+#Infinite
+def factorial(x):
+    return x * factorial(x-1)
+
+print(factorial(5))
+r
+#Non infinite
+def factorial(x):
+  if x == 1:
+    return 1
+  else: 
+    return x * factorial(x-1)
+    
+print(factorial(5))
