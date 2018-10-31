20c73320595087f56e31071677cce61ecc5a7c64
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-10-31 16:51:20 +0000

    Method in class

---
+++ Java4Learn/Day079.java
@@ -0,0 +1,13 @@
+class MyClass {
+
+    static int sum(int val1, int val2) {
+        return val1 + val2;
+    }
+
+    public static void main(String[ ] args) {
+        int x = sum(3, 5);
+        System.out.println(x);
+    }
+}
+
+//output 8
