ec4adc3d73cb9be2b44054563a413e07eb8f0475
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-11-05 14:07:47 +0000

    Create Day82.py

---
+++ mypy/Day82.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self, color, price):
+        self.color = color
+        self.price = price
+
+var = Car("Z_Black", "1m")
+print(var.color,var.price)
+
+"""
+Z_Black 1m
+"""
