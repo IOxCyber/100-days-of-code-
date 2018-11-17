2146c0c306adf24009a8d8f8b52331919aa144fc
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2018-11-17 17:40:40 +0000

    Update Day090.java

--- Java4Learn/Day090.java
+++ Java4Learn/Day090.java
@@ -24,4 +24,31 @@ class Program {
 /*
 Getters and setters allow us to have control over the values. You may, for example, validate the given value in the setter before actually setting the value.
 
-*/
+*/public class Vehicle {
+    private String color;
+    
+    // Getter
+    public String getColor() {
+        return color;
+    }
+    
+    // Setter
+    public void setColor(String c) {
+        this.color = c;
+    }
+}
+
+class Program {
+    public static void main(String[ ] args) {
+        Vehicle v1 = new Vehicle();
+        v1.setColor("Red");
+        System.out.println(v1.getColor());
+    }
+}
+
+
+/*
+Getters and setters allow us to have control over the values. You may, for example, validate the given value in the setter before actually setting the value.
+
+
+
