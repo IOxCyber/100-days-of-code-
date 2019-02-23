ff5b586dc804990c634a0c5a8b0eebc498939532
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2019-02-24 00:11:31 +0530

    Applet_Program

--- Java4Learn/Day074.java
+++ Java4Learn/Day074.java
@@ -1,1 +1,16 @@
-
+import java.applet.Applet; // from package only one class
+import java.awt.Graphics;
+//import javax.swing.Applet;
+/*
+<applet code="Helloworld" width="400" height="450">
+*/
+public class HelloWorld extends Applet
+{
+//declare a method as Paint 
+@Override
+    public void paint(Graphics g)
+    {
+      //draw a String (x,y) cordinates 
+    g.drawString("Hello World!",30,30);
+   } //End method paint
+}
